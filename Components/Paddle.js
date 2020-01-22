6f90005925b4e31ff7a140d44af1648bb4512484
Matt Beachey <mattbeachey@gmail.com>
2020-01-22 14:06:17 -0600

    Initial commit

---
+++ Components/Paddle.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./paddle.css";
+
+export default function Paddle() {
+  return <div className="paddle" />;
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# block-breaker-the-game
-Created with CodeSandbox
