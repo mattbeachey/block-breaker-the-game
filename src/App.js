cf1dba8d16660b69647124507c448b2661442240
Matt Beachey <mattbeachey@gmail.com>
2020-01-22 14:28:35 -0600

    paddle on the screen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./styles.css";
-import Paddle from "../Components/Paddle";
+import Paddle from "./Components/Paddle";
 
 export default function App() {
   return (
